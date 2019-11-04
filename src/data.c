1dd8aadc11d45d769bc8e726e6672afff12da12f
Alexander Rogachev <sorryforbadname@gmail.com>
2019-11-04 17:57:08 +0300

    Refactored memory operations with macros.

--- src/data.c
+++ src/data.c
@@ -5,36 +5,50 @@
 #include "data.h"
 #include "array_utils.h"
 
+/** Code generation **/
+
+#define GENERIC_ALLOC(T) \
+  T *instance = malloc(sizeof(T)); \
+  if (instance == NULL) { \
+    fprintf(stderr, "Failed to allocate memory for " #T "");\
+    exit(EXIT_FAILURE); \
+  } \
+  memset(instance, 0, sizeof(T)); \
+  return instance;
+
+#define FREE(prop) \
+  if (prop != NULL) { \
+    free(prop); \
+  }
+
+#define FREE_CUSTOM(prop, deinit) \
+  if (prop != NULL) { \
+    deinit(prop); \
+  }
+
+/** Helper functions **/
+
+void free_if_not_empty(void *ptr, void(*deallocator)(void *)) {
+  if (ptr != NULL) {
+    (*deallocator)(ptr);
+  }
+}
+
 /** User data **/
 
 twitch_user *twitch_user_alloc() {
-  twitch_user *user = malloc(sizeof(twitch_user));
-
-  if (user == NULL) {
-    fprintf(stderr, "Failed to allocate memory for twitch_user.");
-    exit(EXIT_FAILURE);
-  }
-
-  memset(user, 0, sizeof(twitch_user));
-  return user;
+  GENERIC_ALLOC(twitch_user)
 }
 
 void twitch_user_free(twitch_user *user) {
-  free(user->id);
-  free(user->display_name);
-  free(user->name);
-  free(user->created_at);
-  free(user->updated_at);
-  free(user->type);
-
-  if (user->bio != NULL) {
-    free(user->bio);
-  }
-
-  if (user->logo != NULL) {
-    free(user->logo);
-  }
-
+  FREE(user->id)
+  FREE(user->display_name)
+  FREE(user->name)
+  FREE(user->created_at)
+  FREE(user->updated_at)
+  FREE(user->type)
+  FREE(user->bio)
+  FREE(user->logo)
   free(user);
 }
 
@@ -45,15 +59,7 @@ void twitch_users_list_free(twitch_user **list, int count) {
 /** Channel data **/
 
 twitch_channel *twitch_channel_alloc() {
-  twitch_channel *channel = malloc(sizeof(twitch_channel));
-
-  if (channel == NULL) {
-    fprintf(stderr, "Failed to allocate memory for twitch_channel.");
-    exit(EXIT_FAILURE);
-  }
-
-  memset(channel, 0, sizeof(twitch_channel));
-  return channel;
+  GENERIC_ALLOC(twitch_channel)
 }
 
 twitch_channel *twitch_channel_init(long long int id, char *game, char *name, char *status, char *display_name) {
@@ -77,74 +83,29 @@ twitch_channel *twitch_channel_init(long long int id, char *game, char *name, ch
 }
 
 void twitch_channel_free(twitch_channel *channel) {
-  if (channel->game != NULL) {
-    free(channel->game);
-  }
-
-  if (channel->name != NULL) {
-    free(channel->name);
-  }
-
-  if (channel->status != NULL) {
-    free(channel->status);
-  }
-
-  if (channel->display_name != NULL) {
-    free(channel->display_name);
-  }
-
-  if (channel->language != NULL) {
-    free(channel->language);
-  }
-
-  if (channel->broadcaster_language != NULL) {
-    free(channel->broadcaster_language);
-  }
-
-  if (channel->url != NULL) {
-    free(channel->url);
-  }
-
-  if (channel->video_banner != NULL) {
-    free(channel->video_banner);
-  }
-
-  if (channel->updated_at != NULL) {
-    free(channel->updated_at);
-  }
-
-  if (channel->created_at != NULL) {
-    free(channel->created_at);
-  }
-
-  if (channel->logo != NULL) {
-    free(channel->logo);
-  }
-
-  if (channel->profile_banner != NULL) {
-    free(channel->profile_banner);
-  }
-
-  if (channel->profile_banner_background_color != NULL) {
-    free(channel->profile_banner_background_color);
-  }
-
-  if (channel->broadcaster_software != NULL) {
-    free(channel->broadcaster_software);
-  }
-
-  if (channel->broadcaster_type != NULL) {
-    free(channel->broadcaster_type);
-  }
-
+  FREE(channel->game)
+  FREE(channel->name)
+  FREE(channel->status)
+  FREE(channel->display_name)
+  FREE(channel->language)
+  FREE(channel->broadcaster_language)
+  FREE(channel->url)
+  FREE(channel->video_banner)
+  FREE(channel->updated_at)
+  FREE(channel->created_at)
+  FREE(channel->logo)
+  FREE(channel->profile_banner)
+  FREE(channel->profile_banner_background_color)
+  FREE(channel->broadcaster_software)
+  FREE(channel->broadcaster_type)
   free(channel);
 }
 
 /** Follow data **/
 
 void twitch_follow_free(twitch_follow *follow) {
-  free(follow->created_at);
-  twitch_channel_free(follow->channel);
+  FREE(follow->created_at)
+  FREE_CUSTOM(follow->channel, twitch_channel_free);
   free(follow);
 }
 
@@ -166,15 +127,7 @@ twitch_follow *twitch_follow_init(char *created_at, int notifications, twitch_ch
 }
 
 twitch_stream *twitch_stream_alloc() {
-  twitch_stream *stream = malloc(sizeof(twitch_stream));
-
-  if (stream == NULL) {
-    fprintf(stderr, "Failed to allocate memory for twitch_stream.");
-    exit(EXIT_FAILURE);
-  }
-
-  memset(stream, 0, sizeof(twitch_stream));
-  return stream;
+  GENERIC_ALLOC(twitch_stream)
 }
 
 twitch_stream *twitch_stream_init(long long int id, int average_fps, twitch_channel *channel, char *created_at, int delay, char *game, int is_playlist, int video_height, int viewers) {
@@ -202,16 +155,9 @@ twitch_stream *twitch_stream_init(long long int id, int average_fps, twitch_chan
 }
 
 void twitch_stream_free(twitch_stream *stream) {
-  free(stream->created_at);
-
-  if (stream->game != NULL) {
-    free(stream->game);
-  }
-
-  if (stream->channel != NULL) {
-    twitch_channel_free(stream->channel);
-  }
-
+  FREE(stream->created_at)
+  FREE(stream->game)
+  FREE_CUSTOM(stream->channel, twitch_channel_free);
   free(stream);
 }
 
@@ -220,51 +166,22 @@ void twitch_streams_list_free(twitch_stream **list, int count) {
 }
 
 twitch_summary *twitch_summary_alloc() {
-  twitch_summary *summary = malloc(sizeof(twitch_summary));
-
-  if (summary == NULL) {
-    fprintf(stderr, "Failed to allocate memory for twitch_summary struct.");
-    exit(EXIT_FAILURE);
-  }
-
-  memset(summary, 0, sizeof(twitch_summary));
-  return summary;
+  GENERIC_ALLOC(twitch_summary)
 }
-
 
 void twitch_summary_free(twitch_summary *summary) {
   free(summary);
 }
 
 twitch_featured_stream *twitch_featured_stream_alloc() {
-   twitch_featured_stream *stream = malloc(sizeof(twitch_featured_stream));
-
-  if (stream == NULL) {
-    fprintf(stderr, "Failed to allocate memory for twitch_featured_stream struct.");
-    exit(EXIT_FAILURE);
-  }
-
-  memset(stream, 0, sizeof(twitch_featured_stream));
-  return stream;
+  GENERIC_ALLOC(twitch_featured_stream)
 }
 
 void twitch_featured_stream_free(twitch_featured_stream *stream) {
-  if (stream->image != NULL) {
-    free(stream->image);
-  }
-
-  if (stream->text != NULL) {
-    free(stream->text);
-  }
-
-  if (stream->title != NULL) {
-    free(stream->title);
-  }
-
-  if (stream->stream != NULL) {
-    twitch_stream_free(stream->stream);
-  }
-
+  FREE(stream->image)
+  FREE(stream->text)
+  FREE(stream->title)
+  FREE_CUSTOM(stream->stream, twitch_stream_free);
   free(stream);
 }
 
